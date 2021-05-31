127aad0d662010d76cd397d998ef8dab6c629d6c
Sebastian Tuquerrez <joangomezsebas@gmail.com>
2021-05-31 16:20:30 -0500

    Multilinea

--- src/index.js
+++ src/index.js
@@ -24,3 +24,16 @@ let sinES6 = L + " " + C;
 console.log(sinES6);
 let conES6 = `${L} ${C}`;
 console.log(conES6);
+//#endregion
+
+//#region Multilinea
+let txtSinES6 = "Aprendo las caracteristicas de ECMAscript 6+ \n"
++ "con Oscar Barajas";
+
+let txtConES6 = `Caracteristica
+multilinea
+`;
+
+console.log(txtSinES6);
+console.log(txtConES6);
+//#endregion
