6fce9083d446d4c62db504844b9f8ca34ad9cfae
Sebastian Tuquerrez <joangomezsebas@gmail.com>
2021-05-31 22:22:47 -0500

    Async Await ECMAscript 8

--- src/ES8/index.js
+++ src/ES8/index.js
@@ -22,3 +22,31 @@ const string = 'hello';
 
 console.log(string.padStart(20, 'the earth, say: '), string.padEnd(12, '-------'))
 //#endregion
+
+//#region Async Await
+const helloWorld = () => {
+    return new Promise((resolve, reject) => {
+    (false) //o false
+            ? setTimeout(() => resolve('Hello World'), 3000)
+            :reject(new Error('Test Error'))
+    }) 
+};
+
+const helloAsync = async () => {
+    const hello = await helloWorld();
+    console.log(hello);
+}
+
+helloAsync();
+
+const anotherFunction = async () => {
+    try {
+        const hello = await helloWorld();
+        console.log(hello);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+anotherFunction();
+//#endregion
