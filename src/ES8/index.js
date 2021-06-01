f60370dffdb8e943158e21446213fea0e3b23f4d
Sebastian Tuquerrez <joangomezsebas@gmail.com>
2021-05-31 21:24:02 -0500

    Object Values

--- src/ES8/index.js
+++ src/ES8/index.js
@@ -9,3 +9,11 @@ const entries = Object.entries(data);
 console.log(entries);
 console.log(entries.length);
 //#endregion
+
+//#region Object Values
+//Usando la variable data de la region anterior
+
+const values = Object.values(data);
+console.log(values);
+console.log(values.length);
+//#endregion
