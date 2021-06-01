ca83d6efb314654c23f2eb8be8f5d1fdf717be63
Sebastian Tuquerrez <joangomezsebas@gmail.com>
2021-05-31 23:31:28 -0500

    Propagation Properties

--- src/ES9/index.js
+++ src/ES9/index.js
@@ -1,5 +1,5 @@
 //#region Spread Operator o de Reposo
-const obj = {
+const person = {
     name: 'Sebastian',
     age: 18,
     country: 'CO'
@@ -7,4 +7,18 @@ const obj = {
 
 let { country, ...all } = obj; // o let { name, ...all }
 console.log(all);
+//#endregion
+
+//#region Propagation Properties
+const obj = {
+    name: 'Sebastian',
+    age: 18,
+}
+
+const obj1 = {
+    ...obj,
+    country: 'CO'
+}
+
+console.log(obj1);
 //#endregion
