9375d43f2670b31bfd22ce039c36ea4ca9e6bfbd
Sebastian Tuquerrez <joangomezsebas@gmail.com>
2021-06-01 00:00:36 -0500

    Regex

--- src/ES9/index.js
+++ src/ES9/index.js
@@ -37,3 +37,12 @@ console.log(obj1);
         .catch(error => console.log(error))
         .finally(() => console.log('Finalizo'))
 //#endregion
+
+//#region Regex
+const regexData = /([0-9]{4})-([0-9]{2})-([0-9]{2})/;
+const match = regexData.exec('2019-02-28');
+const year = match[1];
+const month = match[2];
+const day = match[3];
+console.log('Date -> ', year, month, day);
+//#endregion
