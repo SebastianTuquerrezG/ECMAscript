e8f3f7d248d90526d7eb26b34d9fa7900fe302aa
Sebastian Tuquerrez <joangomezsebas@gmail.com>
2021-06-01 13:23:28 -0500

    TrimStar y TrimEnd

--- src/ES10/index.js
+++ src/ES10/index.js
@@ -11,3 +11,11 @@ let array = [1, 2, 3, [1, 2, 3, [1, 2, 3]]];
 
 console.log(array.flatMap(value => [value, value * 2]));
 //#endregion
+
+//#region String TrimStar y TrimEnd
+let saludo = '                 hola, Sebastian.             ';
+
+console.log(saludo);
+console.log(saludo.trimStart());
+console.log(saludo.trimEnd());
+//#endregion
