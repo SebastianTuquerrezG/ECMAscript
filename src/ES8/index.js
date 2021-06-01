a815efaa59a47fd2827e56d9a6c4f3346023b4de
Sebastian Tuquerrez <joangomezsebas@gmail.com>
2021-05-31 21:19:38 -0500

    Object Entries

---
+++ src/ES8/index.js
@@ -0,0 +1,11 @@
+//#region  Object Entries
+const  data = {
+    dataSciencie : 'Sebastian',
+    devOps: 'Joan',
+    UX : 'Tomy'
+} 
+
+const entries = Object.entries(data);
+console.log(entries);
+console.log(entries.length);
+//#endregion
