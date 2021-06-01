e183378faee4605ab5fa260a2edbca16cb0f9ec5
Sebastian Tuquerrez <joangomezsebas@gmail.com>
2021-06-01 12:57:02 -0500

    Array FlatMap

--- src/ES10/index.js
+++ src/ES10/index.js
@@ -4,3 +4,10 @@ let array = [1, 2, 3, [1, 2, 3, [1, 2, 3]]];
 
 console.log(array.flat(4));
 //#endregion
+
+
+//#region Array FlatMap
+let array = [1, 2, 3, [1, 2, 3, [1, 2, 3]]];
+
+console.log(array.flatMap(value => [value, value * 2]));
+//#endregion
