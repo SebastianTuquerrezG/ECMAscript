8f62aef77a68150b27722e0b9b422f221a58d2bb
Sebastian Tuquerrez <joangomezsebas@gmail.com>
2021-05-31 20:51:16 -0500

    Includes ECMAscript7

---
+++ src/ES7/index.js
@@ -0,0 +1,10 @@
+//#region Includes
+let numbers = [28, 2, 19];
+
+if(numbers.includes(28)) {
+    console.log(`Fecha ${numbers[0]}, ${numbers[1]}, ${numbers[2]}` );
+} else{
+    console.log("no es una fecha");
+}
+//Antes se usaba el indexof
+//#endregion
